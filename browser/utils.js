5aa2c542b6a11b3f4d86d97d0c886de924d1bf02
Mickey Burks <brks.mck@gmail.com>
2018-06-16 14:30:19 -0500

    Do proper dir checking, make files fetching more performant, support files with no extension

--- browser/utils.js
+++ browser/utils.js
@@ -2,6 +2,8 @@ import { join } from 'path'
 import { promisify } from 'util'
 import { default as fs, readdir } from 'fs'
 
+const stat = promisify(fs.stat)
+
 export const readDir = promisify(readdir)
 
 export const readFileSync = path => {
@@ -16,35 +18,55 @@ export function getRoot () {
   }
 }
 
-// TODO: isDir and isMd are not checking file type with fs.stat
-const isDir = name => /^[^.]+$/.test(name)
 const isMd = name => /\.md$/.test(name)
-const isNotDotfile = name => /^[^.]/.test(name)
-const isDirOrMd = name => (isDir(name) || isMd(name))
+const hasNoExt = name => /^[^.]+$/.test(name)
+const isDotfile = name => /^\./.test(name)
+const hasMdOrNoExt = name => (isMd(name) || hasNoExt(name))
+const isNotNull = val => (val !== null)
+
+async function isDir (path) {
+  const stats = await stat(path)
+  return stats.isDirectory()
+}
 
 export async function getFiles (path = '') {
   let error = null
   let files = []
 
   try {
-    const contents = await readDir(join(getRoot(), path))
+    path = join(getRoot(), path)
+    const dirContents = await readDir(path)
 
-    files = contents
-      .filter(isNotDotfile)
-      .filter(isDirOrMd)
-      .map(name => {
-        return {
-          name,
-          path: join(__dirname, name),
-          isDir: isDir(name)
+    files = dirContents
+      .map(async name => {
+        // Remove dotfiles
+        if (isDotfile(name)) {
+          return null
         }
+
+        const result = {
+          isMd: isMd(name)
+        }
+
+        // Only allow .md or files without extensions
+        if (!(result.isMd || hasNoExt(name))) {
+          return null
+        }
+
+        result.name = name
+        result.path = join(path, name)
+        result.isDir = await isDir(result.path)
+
+        return result
       })
+      .filter(isNotNull)
 
   } catch (e) {
     console.error(e)
   }
 
-  return files
+  return Promise.all(files)
+    .then(files => files.filter(isNotNull))
 }
 
 export const prevent = e => (e.preventDefault(), true)
