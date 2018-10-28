1c12835bfe3acaace2340d93f02a68fee91fdc33
Mickey Burks <brks.mck@gmail.com>
2018-10-28 14:04:36 -0500

    Reformat Help component

--- app/browser/views/Help/Help.jsx
+++ app/browser/views/Help/Help.jsx
@@ -13,74 +13,60 @@ export default function Help () {
         </tr>
       </thead>
       <tbody>
-        <tr>                        
+        <tr>
           <td className="preformatted">*Italic*</td>
           <td className="preformatted second-example">_Italic_</td>
           <td>
-            <Markdown>
-              *Italic*
-            </Markdown>
+            <Markdown>*Italic*</Markdown>
           </td>
         </tr>
-        <tr>                        
+        <tr>
           <td className="preformatted">**Bold**</td>
           <td className="preformatted second-example">__Bold__</td>
           <td>
-            <Markdown>
-              **Bold**
-            </Markdown>
+            <Markdown>**Bold**</Markdown>
           </td>
         </tr>
         <tr>
-          <td className="preformatted">
-            # Heading 1
-          </td>
+          <td className="preformatted"># Heading 1</td>
           <td className="preformatted second-example">
-            Heading 1<br/>
+            Heading 1<br />
             =========
           </td>
           <td>
-            <Markdown>
-              # Heading 1
-            </Markdown>
+            <Markdown># Heading 1</Markdown>
           </td>
         </tr>
         <tr>
-          <td className="preformatted">
-            ## Heading 2
-          </td>
+          <td className="preformatted">## Heading 2</td>
           <td className="preformatted second-example">
-            Heading 2<br/>
+            Heading 2<br />
             ---------
           </td>
           <td>
-            <Markdown>
-              ## Heading 2
-            </Markdown>
-          </td>
-        </tr>
-        <tr>                        
-          <td className="preformatted">
-            [Link](http://a.com)
-          </td>
-          <td className="preformatted second-example">
-            [Link][1]<br/>
-            &#8942;<br/>
-            [1]: http://b.org
-          </td>
-          <td>
-            <Markdown>
-              [Link](http://a.com)
-            </Markdown>
+            <Markdown>## Heading 2</Markdown>
           </td>
         </tr>
         <tr>
-          <td className="preformatted">
-            ![Image](http://url/a.png)
-          </td>
+          <td className="preformatted">[Link](http://a.com)</td>
           <td className="preformatted second-example">
-            ![Image][1]<br/>
-            &#8942;<br/>
+            [Link][1]
+            <br />
+            &#8942;
+            <br />
+            [1]: http://b.org
+          </td>
+          <td>
+            <Markdown>[Link](http://a.com)</Markdown>
+          </td>
+        </tr>
+        <tr>
+          <td className="preformatted">![Image](http://url/a.png)</td>
+          <td className="preformatted second-example">
+            ![Image][1]
+            <br />
+            &#8942;
+            <br />
             [1]: http://url/b.jpg
           </td>
           <td>
@@ -90,101 +76,107 @@ export default function Help () {
           </td>
         </tr>
         <tr>
-          <td className="preformatted">
-            &gt; Blockquote
-          </td>
-          <td className="preformatted second-example">
-            &nbsp;
-          </td>
+          <td className="preformatted">&gt; Blockquote</td>
+          <td className="preformatted second-example">&nbsp;</td>
           <td>
-            <Markdown>
-              > Blockquote
-            </Markdown>
+            <Markdown>> Blockquote</Markdown>
           </td>
         </tr>
         <tr>
           <td className="preformatted">
             <p>
-              * List<br/>
-              * List<br/>
               * List
+              <br />* List
+              <br />* List
             </p>
           </td>
           <td className="preformatted second-example">
             <p>
-              - List<br/>
-              - List<br/>
-              - List<br/>
+              - List
+              <br />- List
+              <br />- List
+              <br />
             </p>
           </td>
           <td>
-            <Markdown markdown={"* List\n* List\n* List"} />
+            <Markdown markdown={'* List\n* List\n* List'} />
           </td>
         </tr>
         <tr>
           <td className="preformatted">
             <p>
-              1. One<br/>
-              2. Two<br/>
+              1. One
+              <br />
+              2. Two
+              <br />
               3. Three
             </p>
           </td>
           <td className="preformatted second-example">
             <p>
-              1) One<br/>
-              2) Two<br/>
+              1) One
+              <br />
+              2) Two
+              <br />
               3) Three
             </p>
           </td>
           <td>
-            <Markdown markdown={"1. One\n2. Two\n3. Three"} />
+            <Markdown markdown={'1. One\n2. Two\n3. Three'} />
           </td>
         </tr>
         <tr>
           <td className="preformatted">
-            Horizontal Rule<br/>
-            <br/>
+            Horizontal Rule
+            <br />
+            <br />
             ---
           </td>
           <td className="preformatted second-example">
-            Horizontal Rule<br/>
-            <br/>
+            Horizontal Rule
+            <br />
+            <br />
             ***
           </td>
           <td>
-            <Markdown markdown={"Horizontal Rule\n\n---"} />
+            <Markdown markdown={'Horizontal Rule\n\n---'} />
           </td>
         </tr>
-        <tr>                        
-          <td className="preformatted">
-            `Inline code` with backticks
-          </td>
-          <td className="preformatted second-example">
-            &nbsp;
-          </td>
+        <tr>
+          <td className="preformatted">`Inline code` with backticks</td>
+          <td className="preformatted second-example">&nbsp;</td>
           <td>
-            <Markdown>
-              `Inline code` with backticks
-            </Markdown>
+            <Markdown>`Inline code` with backticks</Markdown>
           </td>
         </tr>
         <tr>
           <td className="preformatted">
-            ```<br/>
-            # code block<br/>
-            print '3 backticks or'<br/> 
-            print 'indent 4 spaces'<br/>                            
+            ```
+            <br /># code block
+            <br />
+            print '3 backticks or'
+            <br />
+            print 'indent 4 spaces'
+            <br />
             ```
           </td>
           <td className="preformatted second-example">
-            <span className="spaces">····</span># code block<br/> 
-            <span className="spaces">····</span>print '3 backticks or'<br/>
-            <span className="spaces">····</span>print 'indent 4 spaces'
+            <span className="spaces">····</span># code block
+            <br />
+            <span className="spaces">····</span>
+            print '3 backticks or'
+            <br />
+            <span className="spaces">····</span>
+            print 'indent 4 spaces'
           </td>
           <td>
-            <Markdown markdown={"```\n# code block\nprint '3 backticks or'\nprint 'indent 4 spaces'\n```"} />
+            <Markdown
+              markdown={
+                "```\n# code block\nprint '3 backticks or'\nprint 'indent 4 spaces'\n```"
+              }
+            />
           </td>
-        </tr>                    
+        </tr>
       </tbody>
     </table>
   )
